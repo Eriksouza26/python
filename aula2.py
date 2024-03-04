a8ae95bdc028274f2986958afac7b309cb8c6a36
Eriksouza26 <souzaerik373@gmail.com>
2024-03-04 20:33:15 -0300

    aula 2

---
+++ aula2.py
@@ -0,0 +1,28 @@
+'''
+calculos em python
+em python segue a ordem das operaçoes 
+exponencial = **
+parenteses = (2+2)
+multiplicaçao = *
+divisao = /
+soma e subtraçao = + -
+exemplos:
+
+calculo1= (2+2)**3 * 4 /2 + 3 -1 
+print (calculo1)
+nesse exemplo ele primeiro faz o exponencial para partir para as outras operações onde o resultado é 130.
+
+calculo2= (2+2) * 4 /2 + 3 -1 
+print (calculo2)
+nesse exemplo ele primeiro faz o parenteses para partir para as outras operações onde o resultado é 10.
+
+calculo3= 2 * 4 /2 + 3 -1 
+print (calculo3)
+nesse exemplo ele primeiro faz a multiplicaçao e divisao(na ordem que estiver) para partir para as outras operações onde o resultado é 6.
+
+calculo4= 2 + 3 -1 
+print (calculo4)
+nesse exemplo ele primeiro faz a soma e subtração em ordem que estiver  onde o resultado é 4.
+
+'''
+
