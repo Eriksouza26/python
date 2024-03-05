4e228cf9cca207eda158e1adb9c420084fd514e0
Eriksouza26 <souzaerik373@gmail.com>
2024-03-04 21:37:35 -0300

    AULA 2

--- aula2.py
+++ aula2.py
@@ -65,8 +65,101 @@ print(op)
 #vale lembrar que ele difere de letra maiscula com letra minuscula
 
 
+#terceiro operador é o maior que ">" , so serve para numeros, exemplos
+
+op= 10 > 9
+print(op)
+#nesse caso ele verifica se um numero é maior que o outro e retorna true ou false , nesse caso true
+
+op= 10 > 11
+print(op)
+#nesse caso ele retornarar false , pois o numero da direita é maior que o da esquerda
+
+
+#quarto operador é o menor que "<" , so serve para numeros, exemplos
+
+op= 18 < 19
+print(op)
+#nesse caso ele verifica se um numero é menor que o outro e retorna true ou false , nesse caso true
+
+op= 20 < 19
+print(op)
+#nesse caso ele retornarar false , pois o numero da direita é menor que o da esquerda
+
+
+#quinto operador é o maior ou igual  que ">=" , so serve para numeros, exemplos
+
+op= 10 >= 9
+print(op)
+#nesse caso ele verifica se um numero é maior ou igual que o outro e retorna true ou false , nesse caso true
+
+op= 10 >= 11
+print(op)
+#nesse caso ele retornarar false , pois o numero da direita é maior ou diferente que o da esquerda
+
+op= 17 >= 17
+print(op)
+#nesse caso ele retornarar true , pois o numero da direita é igual o da esquerda
+
+#sexto operador é o menor ou igual que "<" , so serve para numeros, exemplos
+
+op= 18 <= 19
+print(op)
+#nesse caso ele verifica se um numero é menor ou igual que o outro e retorna true ou false , nesse caso true
+
+op= 20 <= 19
+print(op)
+#nesse caso ele retornarar false , pois o numero da direita é menor ou diferente que o da esquerda
+
+op= 20 <= 20
+print(op)
+#nesse caso ele retornarar true , pois o numero da direita é igual o da esquerda
+
+
+
+#operadores de atribuiçao
+
+
+#para simplificar algumas operações se usam os operadores de atribuiçao , exemplos:
+
+x= 10  
+#definimos uma variavel x com o valor dez
+
+#normalmente para somar algo a esse valor usamos essa forma
+#x=x+5
+#mas para simplificar usaremos :
+
+#x +=5
+#print(x)
+
+#o mesmo vale para as outras operações:
+#subtração:
+#x=x-5
+
+#x -=5
+#print(x)
+
+#multiplicação:
+#x=x*5
+
+#x *=5
+#print(x)
+
+#divisão:
+#x=x/5
+
+#x /=5
+#print(x)
+
+#existe uma forma de obtermos o resto da divisao com a porcentagem "%", exemplo
+
+#x=x%3
+
+#onde aqui ele retorna "1" que seria o resto da divisao de x por 3 , no caso 10 por 3, que o resultado seria 3 e resto 1
+#simplificando:
+
+x %=3
+print(x)
+
 
 '''
-
-
-
