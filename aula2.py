705c78b9689664fabb6774303236f12b8f69077f
Eriksouza26 <souzaerik373@gmail.com>
2024-03-04 21:09:01 -0300

    #

--- aula2.py
+++ aula2.py
@@ -7,7 +7,6 @@
 #divisao = /
 #soma e subtraçao = + -
 #exemplos:
-'''
 
 calculo1= (2+2)**3 * 4 /2 + 3 -1 
 print (calculo1)
@@ -24,6 +23,50 @@ print (calculo3)
 calculo4= 2 + 3 -1 
 print (calculo4)
 #nesse exemplo ele primeiro faz a soma e subtração em ordem que estiver  onde o resultado é 4.
+
+
+#operadores de comparaçao 
+#primeiro operador é o de igualdade "==",alem de numeros , ele tambem serve parar caracteres, segue os exemplos:
+
+op= 10==10
+print(op)
+#aqui ele verifica se os valores sao iguais e retorna verdadeiro ou falso , nesse caso true.
+
+op= 10==11
+print(op)
+#aqui ele retorna falso.
+
+op= 'banana'=='banana'
+print(op)
+#aqui ele verifica cada caracter e julga para ver se é exatamente igual, nesse caso true
+
+op= 'banana'=='banane'
+print(op)
+#nesse caso apresenta um caracter diferente , logo ele retorna false
+#vale lembrar que ele difere de letra maiscula com letra minuscula
+
+#segundo operador é o diferente de "!=",alem de numeros , ele tambem serve parar caracteres, segue os exemplos:
+
+op= 10!=11
+print(op)
+#aqui ele verifica se os valores sao diferentes e retorna verdadeiro ou falso , nesse caso true.
+
+op= 10!=10
+print(op)
+#aqui ele retorna falso.
+
+op= 'banana'!='banana'
+print(op)
+#aqui ele verifica cada caracter e julga para ver se é diferente, como é exatamente igual ,logo nesse caso e false
+
+op= 'banana'!='banane'
+print(op)
+#nesse caso apresenta um caracter diferente , logo ele retorna true
+#vale lembrar que ele difere de letra maiscula com letra minuscula
+
+
+
+'''
 
 
 
